c0ba5845bc17a959c66b42093870bbb8d84002f7
Matt Glaman <nmd.matt@gmail.com>
2015-01-10 14:18:35 -0600

    Add spinner notification when loading lists.

--- app/scripts/controllers/listCtrl.js
+++ app/scripts/controllers/listCtrl.js
@@ -4,16 +4,18 @@ projectKanbanApp.controller('listCtrl', ['$scope', '$timeout', 'issueService', f
   $scope.list = $scope.$parent.list;
   $scope.ids = $scope.list.ids;
   $scope.listIssues = [];
+  $scope.processing = true;
 
   var counter = 0;
   var getListIssues = function() {
     if (counter < $scope.ids.length) {
       // That lack of pagination tho.
       issueService.requestIssues($scope.$parent.project.nid, $scope.ids[counter]).then(function(issues) {
-          console.log(issues);
           $scope.listIssues = issues;
+          $scope.processing = false;
         }
       );
+      $timeout(getListIssues, 1000);
     }
   };
   $timeout(getListIssues, 1000);
