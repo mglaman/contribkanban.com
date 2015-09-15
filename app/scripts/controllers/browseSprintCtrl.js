965a8400607681ad05e48461da45bf27c0aca069
Matt Glaman <nmd.matt@gmail.com>
2015-09-14 22:10:07 -0500

    Improve browse page performance for projects and board browsing.

--- app/scripts/controllers/browseSprintCtrl.js
+++ app/scripts/controllers/browseSprintCtrl.js
@@ -1,30 +1,37 @@
 'use strict';
 
 projectKanbanApp.controller('browseSprintCtrl', [
+    '$q',
     '$scope',
     '$routeParams',
     '$location',
     'parseService',
-    function ($scope, $routeParams, $location, parseService) {
+    function ($q, $scope, $routeParams, $location, parseService) {
       $scope.location = $location;
       $scope.routePath = 'sprint';
-      $scope.projects = [];
+      $scope.projecsts = [];
 
-      var parseQuery = parseService.objectQuery('Boards');
-      parseQuery.find({
-        success: function(results) {
-          var rows = [];
-          angular.forEach(results, function(val, key) {
-            rows.push(val.attributes);
-          });
-          angular.copy(rows, $scope.projects);
-          $scope.$apply();
-        },
-        error: function(error) {
+      var querySprints = function () {
+        var deferred = $q.defer();
+        var parseQuery = parseService.objectQuery('Boards');
+        parseQuery.find({
+          success: function(results) {
+            var rows = [];
+            angular.forEach(results, function(val, key) {
+              rows.push(val.attributes);
+            });
+            deferred.resolve(rows);
+          },
+          error: function(error) {
 
-        }
-      })
+          }
+        });
+        return deferred.promise;
+      };
 
+      querySprints().then(function (results) {
+        $scope.projects = results;
+      });
 
     }
   ]
