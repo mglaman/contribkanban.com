45f85009b1bdb038b5bd8583a7d4ef1b9bc99c10
Matt Glaman <nmd.matt@gmail.com>
2015-12-06 22:44:42 -0600

    Set ENV to production, add compression.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var methodOverride = require ('method-override');
 var mongoose = require('mongoose');
+var compression = require('compression');
 
 var db = require('./config/db');
 var port = process.env.PORT || 8080;
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(express.static(__dirname + '/dist'));
+app.use(compression());
 require('./routes')(app);
 app.listen(port); 
 
