9171a7d3c41dd626a5b31f04f9cb742b3e687d6b
Matt Glaman <nmd.matt@gmail.com>
2015-12-09 08:55:54 -0600

    Update db config for specifying local mongo

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
 var platformsh = require("platformsh").config();
-var db = platformsh.relationships.database[0]
+
+var db = {scheme: 'mongodb', username: '', password: '', host: '192.168.99.100', port: '', path: 'contribkanban'}
+if (platformsh !== null) {
+  db = platformsh.relationships.database[0];
+}
 
 module.exports = {
-  prod : db['scheme'] + '://'+ db["username"]+':' + db['password']+ "@" + db['host']+ ":" + db['port']+ '/' + db['path'],
-  local : 'mongodb://192.168.99.100/contribkanban'
+  prod : db['scheme'] + '://' + db["username"] +':' + db['password'] + "@" + db['host'] + ":" + db['port']+ '/' + db['path'],
+  local : db['scheme'] + '://' + db['host']+ ":" + db['port']+ '/' + db['path']
 };
