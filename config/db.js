00ae975973638648e3da0128ed5eb4b971a13f00
Matt Glaman <nmd.matt@gmail.com>
2017-01-04 15:23:54 -0600

    Adjust bower, board height

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 var platformsh = require("platformsh").config();
 
-var db = {scheme: 'mongodb', username: '', password: '', host: '127.0.0.1', port: '27017', path: 'contribkanban'};
+var db = {scheme: 'mongodb', username: 'main', password: 'main', host: 'localhost', port: '30000', path: 'main'};
 if (platformsh !== null) {
   db = platformsh.relationships.database[0];
 }
