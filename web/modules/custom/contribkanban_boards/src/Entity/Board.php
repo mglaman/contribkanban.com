11b851698589658b80b8fe9e11e7561bfdbfad65
Matt Glaman <nmd.matt@gmail.com>
2017-10-17 16:12:57 -0500

    Initial Drupal config.

---
+++ web/modules/custom/contribkanban_boards/src/Entity/Board.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\contribkanban_boards\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+
+class Board extends ContentEntityBase implements BoardInterface {
+
+}
---
+++ web/modules/custom/contribkanban_boards/src/Entity/BoardInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\contribkanban_boards\Entity;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+
+interface BoardInterface extends ContentEntityInterface {
+
+}
