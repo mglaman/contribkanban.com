1a53a1a9165d5f450563d1fd7ef0087e951d98b0
Matt Glaman <nmd.matt@gmail.com>
2017-10-21 16:45:03 -0500

    Use Robo to execute post-deploy commands.

--- RoboFile.php
+++ RoboFile.php
@@ -1,14 +1,33 @@
 <?php
+
 /**
  * This is project's console commands configuration for Robo task runner.
  *
  * @see http://robo.li/
  */
-class RoboFile extends \Robo\Tasks
-{
-    public function runServer() {
-        $this->taskServer(8080)
-        ->dir('web')
-        ->run();
+class RoboFile extends \Robo\Tasks {
+
+  public function runServer() {
+    $this->taskServer(8080)
+      ->dir('web')
+      ->run();
+  }
+
+  public function deploy() {
+    $this->drush('cr')->run();
+    $this->drush('cim', TRUE)->run();
+    $this->drush('updatedb', TRUE)->run();
+    $this->drush('entup', TRUE)->run();
+  }
+
+
+  protected function drush($command, $quiet = FALSE) {
+    $task = $this->taskExec(__DIR__ . '/bin/drush');
+    $task->arg($command);
+    $task->rawArg(sprintf('-r %s/web', __DIR__));
+    if ($quiet) {
+      $task->option('yes');
     }
+    return $task;
+  }
 }
