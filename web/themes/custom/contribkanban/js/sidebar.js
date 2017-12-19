9da1f6b1bdadef3ff3994c9f41050cad28efdc21
Matt Glaman <nmd.matt@gmail.com>
2017-12-19 05:44:43 -0600

    Expand sidebar from header

--- web/themes/custom/contribkanban/js/sidebar.js
+++ web/themes/custom/contribkanban/js/sidebar.js
@@ -1,8 +1,13 @@
 (function (domready) {
   domready(function () {
+    var $appHeader = document.querySelector('.app-header');
     var $appSidebar = document.querySelector('.app-sidebar');
     var $appBackdrop = document.querySelector('.app-sidebar__backdrop');
     var $trigger = document.querySelector('[data-drupal-selector="app-siderbar-trigger"]');
+    $appHeader.addEventListener('click', function() {
+      $appBackdrop.style.display = 'block';
+      $appSidebar.style.display = 'block';
+    })
     $trigger.addEventListener('click', function () {
       $appBackdrop.style.display = 'block';
       $appSidebar.style.display = 'block';
