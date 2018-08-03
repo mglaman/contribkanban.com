944f41e817b4e7e05d54468d3c794478f1fb8ef8
Matt Glaman <nmd.matt@gmail.com>
2018-08-02 21:15:59 -0500

    Remove old code

--- legacy/app/scripts/controllers/modalCtrl.js
+++
@@ -1,9 +0,0 @@
-(function (angular) {
-  'use strict';
-
-  angular.module('appContribkanban').controller('ModalController', ['$scope', 'close', function ($scope, close) {
-    $scope.close = function () {
-      close({}, 200);
-    };
-  }]);
-})(window.angular);
--- legacy/models/sprint.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-module.exports = mongoose.model('Sprint', {
-  machine_name : String,
-  title: String,
-  nid: String,
-  listConfig: Array,
-  tid: Array
-});
