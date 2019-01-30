f1763b92e0827304239156c8e17761919eba3161
Matt Glaman <nmd.matt@gmail.com>
2019-01-29 20:28:26 -0600

    Use updated webpack build (#118)
    * Use updated webpack build
    
    * Fix externals reference

--- web/modules/custom/contribkanban_boards/lib/reducers/issues.js
+++ web/modules/custom/contribkanban_boards/lib/reducers/issues.js
@@ -2,7 +2,6 @@ const issues = (state = [], action) => {
   switch (action.type) {
     case 'REQUEST_LIST_ISSUES':
     case 'RECEIVE_LIST_ISSUES':
-      debugger;
       return action;
     default:
       return state;
