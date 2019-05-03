5bc6c760c8487916b4356b3b0edac26a5436007c
Matt Glaman <nmd.matt@gmail.com>
2019-05-02 21:07:52 -0500

    Debug environment variable in post-rollout

--- scripts/github-build-status.sh
+++ scripts/github-build-status.sh
@@ -9,4 +9,10 @@ if [[ -z ${LAGOON_GIT_SHA} ]]; then
     exit 0
 fi
 
+if [[ -z ${GITHUB_TOKEN} ]]; then
+    echo "No Github token :("
+    env
+    exit 1
+fi
+
 curl https://api.github.com/repos/mglaman/contribkanban.com/statuses/${LAGOON_GIT_SHA} -X POST -H "Content-Type: application/json" -H "Authorization: token ${GITHUB_TOKEN}" --data '{"state": "'${1}'", "target_url": "'${LAGOON_ROUTE}'", "description": "The environment is '${1}'", "context": "amazeeio/lagoon"}'
