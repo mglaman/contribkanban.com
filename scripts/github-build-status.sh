6398c8a409d63dc403750e9b54aae08769dbcad6
Matt Glaman <nmd.matt@gmail.com>
2019-05-02 21:29:48 -0500

    Add Status API calls for Lagoon build (#146)
    * Add Status API calls for deployment
    
    * Fix command
    
    * Adjust the deployment script
    
    * Try to debug failures
    
    * Moar debug.
    
    * Comment out pre-rollout to try and fix build
    
    * Debug environment variable in post-rollout
    
    * Changed env behind scenes to global; make sure always fails
    
    * Add back pending status check now that all is groovy

---
+++ scripts/github-build-status.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ -z ${1} ]]; then
+    echo "Provide a state"
+    exit 1
+fi
+if [[ -z ${LAGOON_GIT_SHA} ]]; then
+    echo "Cannot detect LAGOON_GIT_SHA"
+    exit 1
+fi
+
+if [[ -z ${GITHUB_TOKEN} ]]; then
+    echo "No Github token :("
+    env
+    exit 1
+fi
+
+curl https://api.github.com/repos/mglaman/contribkanban.com/statuses/${LAGOON_GIT_SHA} -X POST -H "Content-Type: application/json" -H "Authorization: token ${GITHUB_TOKEN}" --data '{"state": "'${1}'", "target_url": "'${LAGOON_ROUTE}'", "description": "The environment is '${1}'", "context": "amazeeio/lagoon"}'
