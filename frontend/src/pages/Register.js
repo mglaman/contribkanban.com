88296bea0c6876fcb6597b74ce36deb4e3f8c1f1
Matt Glaman <nmd.matt@gmail.com>
2020-04-08 23:24:29 -0500

    scaffold the register form

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,5 +1,12 @@
-import React from "react";
-import { Container, TextField, Button, Grid, Link } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  TextField,
+  Button,
+  Grid,
+  Link,
+  Typography,
+} from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 import { Link as RouterLink } from "react-router-dom";
 import usePageTitle from "../hooks/pageTitle";
@@ -16,9 +23,32 @@ const styles = (theme) => ({
 
 function RegisterForm({ classes }) {
   usePageTitle("Register");
+  const [errorMessage, setErrorMessage] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
+  const [drupalorgUsername, setDrupalorgUsername] = useState();
+
+  useEffect(() => {
+    setPasswordsMatch(password === confirmPassword);
+  }, [password, confirmPassword]);
+
   return (
     <Container maxWidth="xs">
-      <form className={classes.form}>
+      <form
+        className={classes.form}
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (!passwordsMatch) {
+            setErrorMessage("Passwords do not match");
+          } else {
+          }
+        }}
+      >
+        {errorMessage ? (
+          <Typography color="error">{errorMessage}</Typography>
+        ) : null}
         <TextField
           variant="outlined"
           margin="normal"
@@ -28,7 +58,10 @@ function RegisterForm({ classes }) {
           label="Email Address"
           name="email"
           autoComplete="email"
+          type="email"
           autoFocus
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
         <TextField
           variant="outlined"
@@ -40,6 +73,8 @@ function RegisterForm({ classes }) {
           type="password"
           id="password"
           autoComplete="new-password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
         />
         <TextField
           variant="outlined"
@@ -51,6 +86,10 @@ function RegisterForm({ classes }) {
           type="password"
           id="confirm_password"
           autoComplete="new-password"
+          error={!passwordsMatch}
+          helperText={!passwordsMatch ? `The passwords do not match` : null}
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
         />
         <TextField
           variant="outlined"
@@ -59,6 +98,8 @@ function RegisterForm({ classes }) {
           name="drupalorg_username"
           label="Drupal.org username"
           helperText="Your Drupal.org username, used to provide a board based on issues you follow."
+          value={drupalorgUsername}
+          onChange={(event) => setDrupalorgUsername(event.target.value)}
         />
         <Button
           type="submit"
