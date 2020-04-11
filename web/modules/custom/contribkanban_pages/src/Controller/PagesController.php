878aa63f253252322830f645648d9dcaa55eec7b
Matt Glaman <nmd.matt@gmail.com>
2020-04-11 15:11:28 -0500

    fix warning on 404 page

--- web/modules/custom/contribkanban_pages/src/Controller/PagesController.php
+++ web/modules/custom/contribkanban_pages/src/Controller/PagesController.php
@@ -3,19 +3,16 @@
 namespace Drupal\contribkanban_pages\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Render\Markup;
-use Drupal\Core\Routing\RouteMatch;
-use Drupal\Core\Routing\RouteMatchInterface;
-use Symfony\Component\HttpFoundation\Request;
 
-class PagesController extends ControllerBase {
+class PagesController extends ControllerBase
+{
 
-  public function on404(\Exception $exception, Request $request, RouteMatchInterface $match) {
+  public function on404(\Exception $exception = null)
+  {
     return [
       '#theme' => 'page_not_found',
-      '#previous' => get_class($exception->getPrevious()),
+      '#previous' => $exception ? get_class($exception->getPrevious()) : null,
       '#message' => '',
     ];
   }
-
 }
