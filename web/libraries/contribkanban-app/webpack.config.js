dc802195fafa4ba41d913bc2fa3b6a7fe06a7a07
Matt Glaman <nmd.matt@gmail.com>
2020-04-13 13:35:58 -0500

    Leverage yarn workspaces

---
+++ web/libraries/contribkanban-app/webpack.config.js
@@ -0,0 +1,22 @@
+path = require("path");
+module.exports = {
+  entry: "./lib",
+  output: {
+    path: path.resolve(__dirname, "./js"),
+  },
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
