14654e9ac6576f3ee7084bd0f291120f3d6d90e3
Matt Glaman <nmd.matt@gmail.com>
2020-04-18 14:24:08 -0500

    fix failures

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import { AuthProvider } from "./context/auth";
 import AuthRoute from "./routing/AuthRoute";
