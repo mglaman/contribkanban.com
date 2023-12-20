cedfb35644d4953030a85ffa6e0515161df7c758
Matt Glaman <nmd.matt@gmail.com>
2023-12-20 15:14:12 -0600

    fix namespace

--- web/modules/custom/contribkanban_users/src/UserIssuesFeed.php
+++ web/modules/custom/contribkanban_users/src/UserIssuesFeed.php
@@ -4,8 +4,13 @@ namespace Drupal\contribkanban_users;
 
 use Drupal\Core\Field\Plugin\Field\FieldType\StringItem;
 use Drupal\user\UserInterface;
-use Zend\Feed\Reader\Reader;
+use Laminas\Feed\Reader\Reader;
 
+/**
+ * @todo uses abandoned service in 9.4.x
+ *
+ * @link https://www.drupal.org/node/3258656
+ */
 class UserIssuesFeed {
   public function fetch(UserInterface $user) {
     if ($user->get('drupalorg_uid')->isEmpty()) {
