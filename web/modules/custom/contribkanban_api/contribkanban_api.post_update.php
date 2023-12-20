5875cc5c91da1df54d2d88f539e431b95c13d6f0
Matt Glaman <nmd.matt@gmail.com>
2023-12-20 16:43:57 -0600

    client secret must be null for password grant

--- web/modules/custom/contribkanban_api/contribkanban_api.post_update.php
+++ web/modules/custom/contribkanban_api/contribkanban_api.post_update.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types = 1);
 
 use Drupal\consumers\Entity\Consumer;
+use Drupal\Core\Entity\EntityRepositoryInterface;
 
 /**
  *
@@ -11,4 +12,13 @@ function contribkanban_api_post_update_ensure_consumer() {
     $initial_consumer->delete();
   }
   contribkanban_api_ensure_default_consumer();
+}
+
+function contribkanban_api_post_update_remove_consumer_secret() {
+  $entity_repository = \Drupal::service('entity.repository');
+  assert($entity_repository instanceof EntityRepositoryInterface);
+  $consumer = $entity_repository->loadEntityByUuid('consumer', 'd4f7c501-cff9-4a3f-bae7-aec1db19456c');
+  assert($consumer instanceof Consumer);
+  $consumer->set('secret', NULL);
+  $consumer->save();
 }
